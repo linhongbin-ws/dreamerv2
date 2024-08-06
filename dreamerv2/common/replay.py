5b74e0a01d92bf4701c58464ec15a8b1cc64aab9
Ben <522181941@qq.com>
2024-08-06 16:29:05 +0800

    add sym replay buffer

--- dreamerv2/common/replay.py
+++ dreamerv2/common/replay.py
@@ -7,133 +7,158 @@ import uuid
 import numpy as np
 import tensorflow as tf
 import random
+from copy import deepcopy
+from rgbd_sym.tool.sym import get_random_transform_params, perturb
 
 class Replay:
 
-  def __init__(
+    def __init__(
       self, directory, capacity=0, ongoing=False, minlen=1, maxlen=0,
-      prioritize_ends=False, free_ram_capacity=-1):
-    self._directory = pathlib.Path(directory).expanduser()
-    self._directory.mkdir(parents=True, exist_ok=True)
-    self._capacity = capacity
-    self._ongoing = ongoing
-    self._minlen = minlen
-    self._maxlen = maxlen
-    self._prioritize_ends = prioritize_ends
-    self._random = np.random.RandomState()
-    # filename -> key -> value_sequence
-    if free_ram_capacity>0:
-      self._complete_eps = load_episodes(self._directory, free_ram_capacity, minlen)
-    else:
-      self._complete_eps = load_episodes(self._directory, capacity, minlen)
-      
-    # worker -> key -> value_sequence
-    self._ongoing_eps = collections.defaultdict(
+      prioritize_ends=False, free_ram_capacity=-1, sym=0,):
+        self._directory = pathlib.Path(directory).expanduser()
+        self._directory.mkdir(parents=True, exist_ok=True)
+        self._capacity = capacity
+        self._ongoing = ongoing
+        self._minlen = minlen
+        self._maxlen = maxlen
+        self._prioritize_ends = prioritize_ends
+        self._random = np.random.RandomState()
+        # filename -> key -> value_sequence
+        if free_ram_capacity>0:
+            self._complete_eps = load_episodes(self._directory, free_ram_capacity, minlen)
+        else:
+            self._complete_eps = load_episodes(self._directory, capacity, minlen)
+
+        # worker -> key -> value_sequence
+        self._ongoing_eps = collections.defaultdict(
         lambda: collections.defaultdict(list))
-    self._total_episodes, self._total_steps = count_episodes(directory)
-    self._loaded_episodes = len(self._complete_eps)
-    self._loaded_steps = sum(eplen(x) for x in self._complete_eps.values())
+        self._total_episodes, self._total_steps = count_episodes(directory)
+        self._loaded_episodes = len(self._complete_eps)
+        self._loaded_steps = sum(eplen(x) for x in self._complete_eps.values())
 
-    self._free_ram_capacity = free_ram_capacity
+        self._free_ram_capacity = free_ram_capacity
+        self._sym = sym
 
-  @property
-  def stats(self):
-    return {
-        'total_steps': self._total_steps,
-        'total_episodes': self._total_episodes,
-        'loaded_steps': self._loaded_steps,
-        'loaded_episodes': self._loaded_episodes,
-    }
 
-  def add_step(self, transition, worker=0):
-    episode = self._ongoing_eps[worker]
-    for key, value in transition.items():
-      episode[key].append(value)
-    if transition['is_last']:
-      self.add_episode(episode)
-      episode.clear()
+    @property
+    def stats(self):
+        return {
+            "total_steps": self._total_steps,
+            "total_episodes": self._total_episodes,
+            "loaded_steps": self._loaded_steps,
+            "loaded_episodes": self._loaded_episodes,
+        }
 
-  def add_episode(self, episode):
-    length = eplen(episode)
-    if length < self._minlen:
-      print(f'Skipping short episode of length {length}.')
-      return
-    self._total_steps += length
-    self._loaded_steps += length
-    self._total_episodes += 1
-    self._loaded_episodes += 1
-    episode = {key: convert(value) for key, value in episode.items()}
-    filename = save_episode(self._directory, episode)
-    if self._free_ram_capacity >0:
-      self._complete_eps = load_episodes(self._directory, self._free_ram_capacity, self._minlen)
-    else:
-      self._complete_eps[str(filename)] = episode 
-      self._enforce_limit()
+    def add_step(self, transition, worker=0):
+        episode = self._ongoing_eps[worker]
+        for key, value in transition.items():
+            episode[key].append(value)
+        if transition['is_last']:
+            self.add_episode(episode)
+            if self._sym:
+              for k in range(self._sym):
+                  _eps = deepcopy(episode)
+                  _eps['image'] = []
+                  _eps['action'] = []
+                  for j in range(len(episode["image"])):
+                      theta, trans, pivot = get_random_transform_params(episode['image'][0].shape)
+                      obs_new, _, action_new, _ = perturb(
+                          episode["image"][j],
+                          None,
+                          episode["action"][j],
+                          theta,
+                          trans,
+                          pivot,
+                          set_theta_zero=False,
+                          set_trans_zero=False,
+                          action_only=False,
+                      )
+                      _eps["image"].append(obs_new)
+                      _eps["action"].append(action_new)
+                  self.add_episode(_eps)
+            episode.clear()
 
-  def dataset(self, batch, length):
-    example = next(iter(self._generate_chunks(length)))
-    dataset = tf.data.Dataset.from_generator(
+    def add_episode(self, episode):
+        length = eplen(episode)
+        if length < self._minlen:
+            print(f'Skipping short episode of length {length}.')
+            return
+        self._total_steps += length
+        self._loaded_steps += length
+        self._total_episodes += 1
+        self._loaded_episodes += 1
+        episode = {key: convert(value) for key, value in episode.items()}
+        filename = save_episode(self._directory, episode)
+        if self._free_ram_capacity >0:
+            self._complete_eps = load_episodes(self._directory, self._free_ram_capacity, self._minlen)
+        else:
+            self._complete_eps[str(filename)] = episode 
+            self._enforce_limit()
+
+    def dataset(self, batch, length):
+        example = next(iter(self._generate_chunks(length)))
+        dataset = tf.data.Dataset.from_generator(
         lambda: self._generate_chunks(length),
         {k: v.dtype for k, v in example.items()},
         {k: v.shape for k, v in example.items()})
-    dataset = dataset.batch(batch, drop_remainder=True)
-    dataset = dataset.prefetch(5)
-    return dataset
+        dataset = dataset.batch(batch, drop_remainder=True)
+        dataset = dataset.prefetch(5)
+        return dataset
 
-  def _generate_chunks(self, length):
-    sequence = self._sample_sequence()
-    while True:
-      chunk = collections.defaultdict(list)
-      added = 0
-      while added < length:
-        needed = length - added
-        adding = {k: v[:needed] for k, v in sequence.items()}
-        sequence = {k: v[needed:] for k, v in sequence.items()}
-        for key, value in adding.items():
-          chunk[key].append(value)
-        added += len(adding['action'])
-        if len(sequence['action']) < 1:
-          sequence = self._sample_sequence()
-      chunk = {k: np.concatenate(v) for k, v in chunk.items()}
-      yield chunk
+    def _generate_chunks(self, length):
+        sequence = self._sample_sequence()
+        while True:
+            chunk = collections.defaultdict(list)
+            added = 0
+            while added < length:
+                needed = length - added
+                adding = {k: v[:needed] for k, v in sequence.items()}
+                sequence = {k: v[needed:] for k, v in sequence.items()}
+                for key, value in adding.items():
+                    chunk[key].append(value)
+                added += len(adding['action'])
+                if len(sequence['action']) < 1:
+                    sequence = self._sample_sequence()
+            chunk = {k: np.concatenate(v) for k, v in chunk.items()}
+            yield chunk
 
-  def _sample_sequence(self):
-    episodes = list(self._complete_eps.values())
-    if self._ongoing:
-      episodes += [
+    def _sample_sequence(self):
+        episodes = list(self._complete_eps.values())
+        if self._ongoing:
+            episodes += [
           x for x in self._ongoing_eps.values()
           if eplen(x) >= self._minlen]
-    episode = self._random.choice(episodes)
-    total = len(episode['action'])
-    length = total
-    if self._maxlen:
-      length = min(length, self._maxlen)
-    # Randomize length to avoid all chunks ending at the same time in case the
-    # episodes are all of the same length.
-    length -= np.random.randint(self._minlen)
-    length = max(self._minlen, length)
-    upper = total - length + 1
-    if self._prioritize_ends:
-      upper += self._minlen
-    index = min(self._random.randint(upper), total - length)
-    sequence = {
+        episode = self._random.choice(episodes)
+        total = len(episode['action'])
+        length = total
+        if self._maxlen:
+            length = min(length, self._maxlen)
+        # Randomize length to avoid all chunks ending at the same time in case the
+        # episodes are all of the same length.
+        length -= np.random.randint(self._minlen)
+        length = max(self._minlen, length)
+        upper = total - length + 1
+        if self._prioritize_ends:
+            upper += self._minlen
+        index = min(self._random.randint(upper), total - length)
+        sequence = {
         k: convert(v[index: index + length])
         for k, v in episode.items() if not k.startswith('log_')}
-    sequence['is_first'] = np.zeros(len(sequence['action']), np.bool)
-    sequence['is_first'][0] = True
-    if self._maxlen:
-      assert self._minlen <= len(sequence['action']) <= self._maxlen
-    return sequence
+        sequence['is_first'] = np.zeros(len(sequence['action']), np.bool)
+        sequence['is_first'][0] = True
+        if self._maxlen:
+            assert self._minlen <= len(sequence['action']) <= self._maxlen
+        return sequence
 
-  def _enforce_limit(self):
-    if not self._capacity:
-      return
-    while self._loaded_episodes > 1 and self._loaded_steps > self._capacity:
-      # Relying on Python preserving the insertion order of dicts.
-      oldest, episode = next(iter(self._complete_eps.items()))
-      self._loaded_steps -= eplen(episode)
-      self._loaded_episodes -= 1
-      del self._complete_eps[oldest]
+    def _enforce_limit(self):
+        if not self._capacity:
+            return
+        while self._loaded_episodes > 1 and self._loaded_steps > self._capacity:
+            # Relying on Python preserving the insertion order of dicts.
+            oldest, episode = next(iter(self._complete_eps.items()))
+            self._loaded_steps -= eplen(episode)
+            self._loaded_episodes -= 1
+            del self._complete_eps[oldest]
 
 
 def count_episodes(directory):
